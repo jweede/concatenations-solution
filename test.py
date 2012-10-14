b6e293d05e60dce243ff704727d17d72439810b0
Jon Wedaman <jweede@gmail.com>
2012-10-14 16:50:57 -0400

    a solution

--- test.py
+++ test.py
@@ -1,228 +1,46 @@
-import os, re, itertools
+import os, re
 
-def concat_test(fn):
-    with open(fn) as ff:
-        testf = ff.readlines()
-        #print testf
-        return triefind(testf[0][:-1], testf[1][:-1])
+class concatMatcher():
+    def __init__(self, words):
+        self.regex = re.compile('|'.join(words))
+        self.num_of_words = len(set(words))
+        self.word_len = len(words[0])
 
-def triefind(searchexp, blob):
-    words = searchexp.split(' ')
-    t = Trie()
-    for w in words:
-        t.add_word(w, w)
-    t.make_automaton()
-    matches = 0
+    # returns the starting index of the match, -1 if no match
+    def match(self, blob, pos, found=set(), orig_pos=0):
+        m = self.regex.match( blob[ pos : pos+self.word_len ] )
+        if m:
+            if m.group(0) in found:
+                return -1
+            else:
+                found.add(m.group(0))
+                if len(found) == self.num_of_words:
+                    return orig_pos
+                else:
+                    return self.match(blob, pos+m.end()
+                                     , found, orig_pos)
+        else:
+            return -1
+
+def ourfind(searchexp, blob):
+    matcher = concatMatcher( searchexp.split(' ') )
     for i in range(len(blob)):
-        if t.match(blob[i:]):
-            matches += 1 
-    return matches
-
-
-# def ourfind(searchexp, blob):
-#     #l = searchexp[:-1].split(' ')
-#     #ol = searchexp[:-1].split(' ')
-#     blob = blob[:-1]
-
-#     def match_rest(pos, words):
-#         #base case - match
-#         if len(words) == 0:
-#             return True
-#         else:
-#             m = re.match( '|'.join( words )
-#                          , blob[pos:]
-#                          )
-#             if m:
-#                 #print m.group(0), pos+m.end()
-#                 words.remove( m.group(0) )
-#                 #print words
-#                 return match_rest( pos+m.end()
-#                                  , words
-#                                  )
-#             #base case - no match
-#             else:
-#                 return False
-#     matches = 0
-#     for i in range(len(blob)):
-#         if match_rest(i, searchexp[:-1].split(' ') ):
-#             matches += 1
-
-#     return matches
+        x = matcher.match(blob, i
+                   ,found=set(), orig_pos=i)
+        if x != -1: # a match!
+            return x
+    return -1
 
 def outfile_dump(out_f):
     with open(out_f) as ff:
-        return ff.read()[:-1]
-
-nil = object()  # used to distinguish from None
-
-class TrieNode(object):
-    __slots__ = ['char', 'output', 'fail', 'children']
-    def __init__(self, char):
-        self.char = char
-        self.output = nil
-        self.fail = nil
-        self.children = {}
-
-    def __repr__(self):
-        if self.output is not nil:
-            return "<TrieNode '%s' '%s'>" % (self.char, self.output)
-        else:
-            return "<TrieNode '%s'>" % self.char
-
-
-class Trie(object):
-    def __init__(self):
-        self.root = TrieNode('')
-
-
-    def __get_node(self, word):
-        node = self.root
-        for c in word:
-            try:
-                node = node.children[c]
-            except KeyError:
-                return None
-
-        return node
-
-
-    def get(self, word, default=nil):
-        node = self.__get_node(word)
-        output = nil
-        if node:
-            output = node.output
-
-        if output is nil:
-            if default is nil:
-                raise KeyError("no key '%s'" % word)
-            else:
-                return default
-        else:
-            return output
-
-
-    def keys(self):
-        for key, _ in self.items():
-            yield key
-
-
-    def values(self):
-        for _, value in self.items():
-            yield value
-
-    
-    def items(self):
-        L = []
-        def aux(node, s):
-            s = s + node.char
-            if node.output is not nil:
-                L.append((s, node.output))
-
-            for child in node.children.values():
-                if child is not node:
-                    aux(child, s)
-
-        aux(self.root, '')
-        return iter(L)
-
-    
-    def __len__(self):
-        stack = [self.root]
-        n = 0
-        while stack:
-            n += 1
-            node = stack.pop()
-            for child in node.children.values():
-                stack.append(child)
-
-        return n
-
-
-    def add_word(self, word, value):
-        if not word:
-            return
-
-        node = self.root
-        for i, c in enumerate(word):
-            try:
-                node = node.children[c]
-            except KeyError:
-                n = TrieNode(c)
-                node.children[c] = n
-                node = n
-
-        node.output = value
-
-
-    def clear(self):
-        self.root = TrieNode('')
-
-
-    def exists(self, word):
-        node = self.__get_node(word)
-        if node:
-            return bool(node.output != nil)
-        else:
-            return False
-
-
-    def match(self, word):
-        return (self.__get_node(word) is not None)
-        
-
-    def make_automaton(self):
-        queue = []
-
-        # 1.
-        for i in range(256):
-            c = chr(i)
-            if c in self.root.children:
-                node = self.root.children[c]
-                node.fail = self.root   # f(s) = 0
-                queue.append(node)
-            else:
-                self.root.children[c] = self.root
-
-        # 2.
-        while queue:
-            r = queue.pop(0);
-            for node in r.children.values():
-                queue.append(node)
-                state = r.fail
-                while node.char not in state.children:
-                        state = state.fail
-
-                node.fail = state.children.get(node.char, self.root)
-
-
-    def find_all(self, string, callback, start=None, end=None):
-        for index, output in self.iter():
-            callback(index, output)
-
-
-    def iter(self, string, start=None, end=None):
-        start = start if start is not None else 0
-        end = end if end is not None else len(string)
-
-        state = self.root
-        for index, c in enumerate(string[start:end]):
-            while c not in state.children:
-                state = state.fail
-
-            state = state.children.get(c, self.root)
-
-            tmp = state
-            output = []
-            while tmp is not nil and tmp.output is not nil:
-                output.append(tmp.output)
-                tmp = tmp.fail
-
-            if output:
-                yield (index + start, output)
+        return ff.read()
 
 if __name__ == '__main__':
-    ins_outs_f = [("input0%d.txt" % i, "output0%d.txt" % i) for i in range(2+1) ]
+    ins_outs_fs = [("input0%d.txt" % i, "output0%d.txt" % i) for i in range(5+1) ]
 
-    for i, o in ins_outs_f:
-        match_count = concat_test( i )
-        print match_count, outfile_dump(o)
+    for i, o in ins_outs_fs:
+        with open(i) as ff:
+            testf = ff.read().split('\n')
+            result = ourfind( testf[0]   # first line: input words 
+                            , testf[1] ) # second line: search blob
+        print "result: %d; expected result: %s;" % (result, outfile_dump(o))
